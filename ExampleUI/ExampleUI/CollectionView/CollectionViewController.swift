816611d892a4da0e0a35a2bf169a628b09a000fc
Andrés Bonilla <abraham.go@outlook.com>
2022-02-21 22:32:55 -0600

    Feature: collection view

---
+++ ExampleUI/ExampleUI/CollectionView/CollectionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CollectionViewController.swift
+//  ExampleUI
+//
+//  Created by Andrés Bonilla Gómez on 21/02/22.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.dataSource = self
+            collectionView.delegate = self
+            collectionView.register(UINib(nibName: "TitleCollectionViewCell", bundle: nil),
+                                    forCellWithReuseIdentifier: "TitleCollectionViewCell")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let flowLayout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
+    }
+}
+
+extension CollectionViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("toco alguna celda")
+        // logica para validar que celda toco? numero y seccion
+        print(indexPath.row)
+    }
+}
